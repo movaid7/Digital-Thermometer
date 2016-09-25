5ebced0c7922d322738cb84b3a772364717d65e4
Muhammad Vaid <molvy32@gmail.com>
2016-09-25 16:30:50 +0200

    Initial

---
+++ build/default/production/214505927P4.o.d
@@ -0,0 +1,3 @@
+build/default/production/214505927P4.o:  \
+	C:/Users/Muhammad/MPLABXProjects/Project4.X/214505927P4.asm  \
+
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Project4.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Project4.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=project4.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/project4.x.tar
